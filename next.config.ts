61c101e5f3c54469ebdf97c9f3521098ac9f7b70
bedro95 <elkorgliplay@gmail.com>
2026-01-09 03:51:09 +0200

    chore: clean up next config for version 16 compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
   typescript: {
+    // Ignore direct TS errors to ensure smooth deployment for now
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // We removed the 'eslint' key from here as per the new update
 };
 
 export default nextConfig;
